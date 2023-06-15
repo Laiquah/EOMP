80042b69a442160f2384f4c07a7517428efa81af
laiquah <cotielaiquah@gmail.com>
2023-06-15 20:04:56 +0200

    navbar hover is added

--- script/checkout.js
+++ script/checkout.js
@@ -19,35 +19,3 @@ function checkoutDisplay() {
 }
 
 checkoutDisplay()
-   
-
-
-// var cart = [];
-
-// function addToCart(id, title, image, description, price) {
-//   var item = {
-//     id: id,
-//     title: title,
-//     image: image,
-//     description: description,
-//     price: price,
-//   };
-
-//     cart.push(item);
-//     updateCart();
-// }
-
-// function updateCart() {
-//   var cartItemsElement = document.querySelector("#cartItems");
-//   cartItemsElement.innerHTML = "";
-
-//     for (var i = 0; i < cart.length; i++) {
-//     var item = cart[i];
-
-//     var li = document.createElement("li");
-
-//     li.innerHTML = item.id + item.title + item.image + item.description + item.price;
-
-//     cartItemsElement.appendChild(li);
-//   }
-// }
